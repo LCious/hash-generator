86e49a2327ae6f5e3c2d568be7d3abe64fe9d4fe
LCious <103113016+LCious@users.noreply.github.com>
2022-11-08 00:18:42 +0200

    File read function

--- Main.cpp
+++ Main.cpp
@@ -9,8 +9,7 @@
 
 using namespace std;
 
-string string_to_hex(const std::string& input) // simboliu konvertavimas i hex'a
-{
+string string_to_hex(const std::string& input){			//CONVERT SYMBOL TO HEX
 	static const char hex_digits[] = "0123456789ABCDEF";
 
 	std::string output;
@@ -23,6 +22,41 @@ string string_to_hex(const std::string& input) // simboliu konvertavimas i hex'a
 	return output;
 }
 
+bool file(string failas, vector<string>& data){			//FILE READ
+	ifstream in(failas.c_str());
+	if (!in){
+		cout << "Tokio failo nera!" << endl;
+		return false;
+	}
+	string line;
+	int n;
+	cout << "Nuskaityti visa faila (0), tam tikra skaiciu eiluciu (1): ";
+	cin >> n;
+	while (n != 0 && n != 1){
+		cout << "Neteisinga ivestis! Nuskaityti visa faila (0), tam tikra skaiciu eiluciu (1): ";
+		cin >> n;
+	}
+	if (n == 0){
+		while (getline(in, line)){
+			data.push_back(line);
+		}
+	}else{
+		cout << "Kiek eiluciu nuskaityti?: ";
+		cin >> n;
+		while (n < 1){
+			cout << "Neteisinga ivestis! Eiluciu skaicius 1..n: ";
+			cin >> n;
+		}
+		for (int i = 0; i < n; i++){
+			getline(in, line);
+			data.push_back(line);
+		}
+	}
+
+	in.close();
+	return true;
+}
+
 int main(int argc, char const* argv[])
 {
 	int input;
@@ -33,13 +67,21 @@ int main(int argc, char const* argv[])
 	string txt;
 	stringstream text;
 	vector<string> col;
-	if (input == 1) // ivestis is failo
-	{
-		//TO DO
-		cout << "Atsiprasome nuskaityti is failo negalima";
-	}
-	else // ivestis ranka
-	{
+	if (input == 1){
+		vector<string> data;
+		string filename;
+		cout << "Iveskite failo pavadinima: ";
+		cin >> filename;
+		filename = filename + ".txt";
+		ifstream input(filename);
+		bool result = file(filename, data);
+		for (auto& line : data){
+			text << line;
+			col.push_back(line);
+			lines++;
+		}
+		txt = text.str();
+	}else if(input ==0){
 		cout << "Iveskite teksta: ";
 		cin.clear();
 		cin.ignore(256, '\n');
@@ -50,18 +92,17 @@ int main(int argc, char const* argv[])
 	string hash = "EE9A17A0F3E783EE983B7EFD23324395FE3A8E0563F860EF72D5420B9414FBEC"; // seed'as
 	int tmp = 0;
 	auto start = std::chrono::steady_clock::now();
-	for (int i = 1; i < 64; i++)										// hash funkcija
-	{
-		for (int j = 0; j < txt.size(); j++)
-		{
-			tmp = hash[i];
-			tmp = tmp ^ int(txt[j]);
-			hash[i] = char(tmp);
-			hash[i - 1] += i ^ j;
+
+		for (int i = 1; i < 64; i++){				//hash function
+			for (int j = 0; j < txt.size(); j++){
+				tmp = hash[i];
+				tmp = tmp ^ int(txt[j]);
+				hash[i] = char(tmp);
+				hash[i - 1] += i ^ j;
+			}
+			hash[i] = int(hash[i]) + int(hash[64 - i]);
 		}
-		hash[i] = int(hash[i]) + int(hash[64 - i]);
-	}
-	auto end = std::chrono::steady_clock::now();
+		auto end = std::chrono::steady_clock::now();
 
 	hash = string_to_hex(hash);
 	hash.resize(64);
