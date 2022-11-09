c99947de8b279b5d0912fafeba0da3504a637c7d
LCious <103113016+LCious@users.noreply.github.com>
2022-11-09 21:57:06 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,303 +1,141 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <chrono>
-#include <random>
-
-
+#include "functions.hpp"
 using namespace std;
 
-string string_to_hex(const std::string &input)
-{ // CONVERT SYMBOL TO HEX
-	static const char hex_digits[] = "0123456789ABCDEF";
-
-	std::string output;
-	output.reserve(input.length() * 2);
-	for (unsigned char c : input)
-	{
-		output.push_back(hex_digits[c >> 4]);
-		output.push_back(hex_digits[c & 15]);
-	}
-	return output;
-}
-
-bool file(string failas, vector<string> &data)
-{ // FILE READ
-	ifstream in(failas.c_str());
-	if (!in)
-	{
-		cout << "Tokio failo nera!" << endl;
-		return false;
-	}
-	string line;
-	int n;
-	cout << "Nuskaityti visa faila (0), tam tikra skaiciu eiluciu (1): ";
-	cin >> n;
-	while (n != 0 && n != 1)
-	{
-		cout << "Neteisinga ivestis! Nuskaityti visa faila (0), tam tikra skaiciu eiluciu (1): ";
-		cin >> n;
-	}
-	if (n == 0)
-	{
-		while (getline(in, line))
-		{
-			data.push_back(line);
-		}
-	}
-	else
-	{
-		cout << "Kiek eiluciu nuskaityti?: ";
-		cin >> n;
-		while (n < 1)
-		{
-			cout << "Neteisinga ivestis! Eiluciu skaicius 1..n: ";
-			cin >> n;
-		}
-		for (int i = 0; i < n; i++)
-		{
-			getline(in, line);
-			data.push_back(line);
-		}
-	}
-
-	in.close();
-	return true;
-}
-
-void generate(int size, int length) {
-	using hrClock = std::chrono::high_resolution_clock;
-	std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
-	std::uniform_int_distribution<int> numb(48, 126);
-
-	stringstream buffer;
-
-	string fileName = to_string(size);
-	fileName += "_" + to_string(length) + ".txt";
-	std::ofstream rf(fileName);
-
-	auto start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < length; j++)
-		{
-			buffer << (char)numb(mt);
-		}
-		if (i < size - 1)
-			buffer << "\n";
-	}
-
-	rf << buffer.str();
-
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> diff = end - start; // Skirtumas (s)
-	std::cout << size << " row size generation time " << diff.count() << " s\n";
-
-	buffer.str("");
-	buffer.clear();
-	rf.close();
-}
-
-void generate(int size) {
-	using hrClock = std::chrono::high_resolution_clock;
-	std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
-	std::uniform_int_distribution<int> numb(48, 126);
-	std::uniform_int_distribution<int> rnd(100, 100);
-
-	stringstream buffer;
-
-	string fileName = to_string(size);
-	fileName += ".txt";
-	std::ofstream rf(fileName);
-
-	auto start = std::chrono::high_resolution_clock::now();
-	int length;
-	string smth;
-	for (int i = 0; i < size; i++)
-	{
-		length = rnd(mt);
-		smth.clear();
-		for (int j = 0; j < length; j++)
-		{
-			smth += (char)numb(mt);
-		}
-		buffer << smth;
-		if (i < size - 1)
-			buffer << "\n";
-		std::uniform_int_distribution<int> change(0, length - 1);
-		smth.at(change(mt)) = '/';
-		buffer << smth;
-		i++;
-		if (i < size - 1)
-			buffer << "\n";
-	}
-
-	rf << buffer.str();
-
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> diff = end - start; // Skirtumas (s)
-	std::cout << size << " row size generation time " << diff.count() << " s\n";
-
-	buffer.str("");
-	buffer.clear();
-	rf.close();
-}
-
-void hash_function(string txt)
+int main(int argc, char const* argv[])
 {
-	string hash = "EE9A17A0F3E783EE983B7EFD23324395FE3A8E0563F860EF72D5420B9414FBEC"; // SEED
-	int tmp = 0;
-	for (int i = 1; i < 64; i++)
-	{ // hash function
-		for (int j = 0; j < txt.size(); j++)
-		{
-			tmp = hash[i];
-			tmp = tmp ^ int(txt[j]);
-			hash[i] = char(tmp);
-			hash[i - 1] += i ^ j;
-		}
-		hash[i] = int(hash[i]) + int(hash[64 - i]);
-	}
-	hash = string_to_hex(hash);
-	hash.resize(64);
-	cout << "Hash:" << endl;
-	cout << hash << endl;
-}
-
-void hash_function_everyline(int lines,vector<string> &col){
-	int tmp = 0;
-	string hash = "EE9A17A0F3E783EE983B7EFD23324395FE3A8E0563F860EF72D5420B9414FBEC"; // SEED
-	for (int i = 0; i < lines; i++)
-	{
-		for (int j = 0; j < 64; j++)
-		{
-			for (int x = 0; x < col[i][col[i].size()]; x++)
-			{
-				tmp = hash[i];
-				tmp = tmp ^ int(col[i][x]);
-				hash[i] = char(tmp);
-				hash[i - 1] += i ^ j;
-			}
-			hash[j] = int(hash[j]) + int(hash[64 - j]);
-		}
-		hash = string_to_hex(hash);
-		hash.resize(64);
-		col[i] = hash;
-		// cout << col[i]; << endl;
-	}
-	cout <<"Eilutciu skaicius: "<< lines;
-}
-
-void collisions(int lines, vector<string> col) {
-	int collision = 0;
-	for (int i = 0; i < lines; i++)
-	{
-	for (int j = 0; j < lines; j++)
-	{
-		if (col[i] == col[j] && i != j)
-		{
-			collision++;
-		} 
-	}
-	}
-	cout << endl << "Koliziju skaicius: " << collision << endl;
-}
-
-int main(int argc, char const *argv[])
-{
-	
-	generate(100000); // skiriasi 1 simbolis
-
     cout << "Pasirinkite testavimo metoda: " << endl
-    << "---------------------------" << endl
-    << "1. Ivedimas i konsole. " << endl
-    << "2. Ivedimas is failo. " << endl
-	<< "3. Palyginimas " << endl
-	<< "4. Failu generavimas " << endl
-	<< "---------------------------" << endl;
-	int input;
-	cin >> input;
-	int lines = 0;
-	string txt;
-	stringstream text;
-	vector<string> col;
-	while (input != 1 && input != 2 && input != 3 && input != 4) {
-		cout << "Ivestis neteisinga, bandykite isnaujo:" << endl;
-		cin >> input;
-	}
-	if (input == 2)
-	{
-		vector<string> data;
-		string filename;
-		cout << "Iveskite failo pavadinima: ";
-		cin >> filename;
-		filename = filename + ".txt";
-		ifstream input(filename);
-		bool result = file(filename, data);
-		for (auto &line : data)
-		{
-			text << line;
-			col.push_back(line);
-			lines++;
-		}
-		txt = text.str();
+        << "---------------------------" << endl
+        << "1. Ivedimas i konsole. " << endl
+        << "2. Ivedimas is failo. " << endl
+        << "3. Palyginimas " << endl
+        << "4. Failu generavimas " << endl
+        << "---------------------------" << endl;
+    int input;
+    cin >> input;
+    int lines = 0;
+    string txt;
+    stringstream text;
+    vector<string> col;
+    while (input != 1 && input != 2 && input != 3 && input != 4)
+    {
+        cout << "Ivestis neteisinga, bandykite isnaujo:" << endl;
+        cin >> input;
+    }
+    if (input == 2)
+    {
+        vector<string> data;
+        string filename;
+        cout << "Pasirinkite faila: " << endl
+            << "---------------" << endl
+            << "a" << endl
+            << "b" << endl
+            << "randomText1" << endl
+            << "randomText2" << endl
+            << "1500symbols_1" << endl
+            << "1500symbols_2" << endl
+            << "empty.txt" << endl
+            << "100000" << endl
+            << "100000_1000" << endl
+            << "konstitucija" << endl
+            << "----------------" << endl;
+        cin >> filename;
+        filename = "InputFiles/" + filename + ".txt";
+        ifstream input(filename);
+        bool result = file(filename, data);
+        for (auto& line : data)
+        {
+            text << line;
+            col.push_back(line);
+            lines++;
+        }
+        txt = text.str();
+        cout << "---------------------------------------------" << endl
+             << "Ar norite atlikti testavimus? (Ne:0,Taip:1): " << endl
+             << "---------------------------------------------" << endl;
+        int input1;
+        cin >> input1;
+        while (input1 != 0 && input1 != 1)
+        {
+            cout << "Neteisinga ivestis! Bandykite isnaujo";
+            cin >> input1;
+        }
+        if (input1 == 0)
+        {
+            hash_function(txt);
+        }
+        else if (input1 == 1)
+        {
+            hash_function_everyline(lines, col,input1);
+            collisions(lines, col);
+            vector<double> percent;
+            for (int i = 0; i < col.size() - 1; i++) {
+                percent.push_back(compare(col.at(i), col.at(i + 1)));
+                i++;
+            }
+            sort(percent.begin(), percent.end());
+            double sum = 0;
+            for (auto& el : percent) {
+                sum += el;
+            }
+            cout << "----------HEXADECIMAL SIMILARITIES-----------" << endl
+            << "minimum similarity: " << percent.at(0) << "%" << endl
+            << "average similarity: " << sum / percent.size() << "%" << endl
+            << "maximum similarity: " << percent.at(percent.size() - 1) << "%" << endl
+            << "---------------------------------------------" << endl;
 
-    		cout << "---------------------------------------------" << endl
-				 << "Ar norite atlikti testavimus? (Ne:0,Taip:1): " << endl
-				 << "---------------------------------------------" << endl;
-			int input1;
-			cin >> input1;
-			while(input1 != 0 && input1 != 1){
-				cout << "Neteisinga ivestis! Bandykite isnaujo";
-				cin >> input1;
-			}
-			if(input1 == 0){
-				auto start = std::chrono::steady_clock::now();
-				hash_function(txt);
-				auto end = std::chrono::steady_clock::now();
-				cout << "Uztruko: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000. << "s" << endl;
-			} else if(input1 == 1){
-				auto start = std::chrono::steady_clock::now();
-				hash_function_everyline(lines, col);
-				collisions(lines, col);
-				auto end = std::chrono::steady_clock::now();
-				cout << "Uztruko: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000. << "s" << endl;
-			}
-
-	}
-	else if (input == 1)
-	{
-		cout << "Iveskite teksta: ";
-		cin.clear();
-		cin.ignore(256, '\n');
-		getline(cin, txt);
-		cout << "Pradinis tekstas: " << endl << txt << endl;
-		hash_function(txt);
-	}
-	else if (input == 3)
-	{
-		"To do comparison";
-	}
-	else if (input == 4)
-	{
-		cout << "Pasirinkite failo generavima: " << endl
-		<< "---------------------------" << endl
-		<< "1. 100000 simboliu ir 1000 eiluciu " << endl
-		<< "2. 100000 simboliu su skirtingu 1 simboliu " << endl
-		<< "---------------------------" << endl;
-		cin >> input;
-		while (input != 1 && input != 2) {
-			cout << "Ivestis neteisinga, bandykite isnaujo:" << endl;
-			cin >> input;
-			if (input == 1) {
-				generate(100000, 1000); // random simboliu eilutes
-			}
-			else if(input ==2){
-				generate(100000);
-			}
-		}
-	}
+            // panasumas BINARY
+            percent.clear();
+            for (int i = 0; i < col.size() - 1; i++) {
+                percent.push_back(compare(hex_to_binary(col.at(i)), hex_to_binary(col.at(i + 1))));
+                i++;
+            }
+            sort(percent.begin(), percent.end());
+            sum = 0;
+            for (auto& el : percent) {
+                sum += el;
+            }
+            cout<< "------------BINARY SIMILARITIES-------------" << endl
+            << "minimum similarity: " << percent.at(0) << "%" << endl
+            << "average similarity: " << sum / percent.size() << "%" << endl
+            << "maximum similarity: " << percent.at(percent.size() - 1) << "%" << endl
+            << "---------------------------------------------" << endl;
+            hex_to_binary(col.at(0));
+        }
+    }
+    else if (input == 1)
+    {
+        cout << "Iveskite teksta: ";
+        cin.clear();
+        cin.ignore(256, '\n');
+        getline(cin, txt);
+        cout << "Pradinis tekstas: " << endl
+        << txt << endl;
+        hash_function(txt);
+    }
+    else if (input == 3)
+    {
+        "To do comparison";
+    }
+    else if (input == 4)
+    {
+        cout << "Pasirinkite failo generavima: " << endl
+        << "-------------------------------------------" << endl
+        << "1. 100000 simboliu ir 1000 eiluciu " << endl
+        << "2. 100000 simboliu su skirtingu 1 simboliu " << endl
+        << "-------------------------------------------" << endl;
+        cin >> input;
+        while (input != 1 && input != 2)
+        {
+            cout << "Ivestis neteisinga, bandykite isnaujo:" << endl;
+            cin >> input;
+        }
+        if (input == 1)
+        {
+            generate(100000, 1000); // random simboliu eilutes
+        }
+        else if (input == 2)
+        {
+            generate(100000); // skiriasi vienas simbolis
+        }
+    }
 }
